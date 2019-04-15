58cd4e19ebdc029d26c19648e595a1c87448bdd6
satanbaby <fm351@yahoo.com.tw>
2019-04-16 00:20:47 +0800

    設置環境Axios及Bootstrap

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+// 在 Vue 使用插件
+// https://vuejs.org/v2/guide/plugins.html
+Vue.use(VueAxios, axios)
+
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,22 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <p>{{msg}}</p>
   </div>
 </template>
+
+<script lang="ts">
+// 測試 Axios 正確運行
+import Vue from 'vue'
+export default Vue.extend({
+  created () {
+    const api = `https://www.ris.gov.tw/rs-opendata/api/v1/datastore/ODRP001/10601`
+    const vm = this
+    ;(async function () {
+      await vm.$http.get(api)
+        .then(response => console.log(response))
+        .catch(err => console.log(err))
+    }())
+    console.log('secondData')
+  }
+})
+</script>
